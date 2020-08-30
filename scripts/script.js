ce233d459f23e0a33c3cc5cba146514696af43b8
AnastasiaStreltsova <anastasiyaSTR1995@gmail.com>
2020-08-30 15:37:20 +0300

    function createCard changed

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@
 const popup = document.querySelectorAll(".popup");
 const popupProfile = document.querySelector(".popup_type_profile");
 const popupAddCard = document.querySelector(".popup_type_add-card");
+
 const popupImage = document.querySelector(".popup_type_image");
+const subtitle = popupImage.querySelector(".popup__subtitle");
 
 
 const profileIntroButtonEdit = document.querySelector(".profile__button-edit");
@@ -13,6 +15,7 @@ const popupButtonCloseImage = document.querySelector(".popup__button-close_type_
 const popupButtonSave = document.querySelector(".popup__button-save");
 const popupButtonAdd = document.querySelector(".profile__button-add");
 const popupButtonCreate = document.querySelector(".popup__button-create");
+const img = popupImage.querySelector(".popup__image");
 
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
@@ -24,6 +27,7 @@ const popupTitle = document.querySelector(".popup__input_type_title");
 const popupLink = document.querySelector(".popup__input_type_link");
 
 const gallery = document.querySelector(".elements");
+
 
 
 const popupShowAddCard = function() {
@@ -57,7 +61,7 @@ function formSubmitHandlerProfile (event) {
 formElement.addEventListener('submit', formSubmitHandlerProfile);
 
 // Массив с объектами, с которым будем работать в функции addСard(добавляя данные);
-let initialCards = [
+const initialCards = [
     {
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -85,71 +89,67 @@ let initialCards = [
 ];
 
 //Создаем карточки и заполняем их данными из массива
-function createCard(arr) {
+function createCard(obj) {
     const card = document.querySelector("#cards")
     const cardTemplate = card.content;
+
+    //функция добавления лайка
+    function addLike(buttonLike) {
+        buttonLike.addEventListener("click", function(event) {
+            event.target.classList.add("element__like_active");
+        })
+    }
     
+    //функция удаления лайка
+    function deleteCard(buttonDeleteCard) {
+        buttonDeleteCard.addEventListener("click", function(event) {
+            const cardActive = this.parentNode;
+            cardActive.parentNode.removeChild(cardActive);     
+        });
+    }
+    
+    //функция открытия попапа с картинкой
+    function openFullPopupImage(cardImageFull) {
+        cardImageFull.addEventListener("click", function(event) {
+            popupImage.classList.add("popup_opened");
+            const cardImageLink = event.target.src;
+            
+            img.src = cardImageLink;
+            
+            subtitle.textContent = this.parentNode.querySelector(".element__title").textContent;
+        })                 
+    }
 
 //пробежались по циклу, для каждого i создается html заготовка. копируются данные из массива в каждую крточку и 
 //вставляется
-    for(i = 0; i < arr.length; i++) {
 
-        const cardEmptyCopy = cardTemplate.cloneNode(true);
+    const cardEmptyCopy = cardTemplate.cloneNode(true);
 
-        const cardName = cardEmptyCopy.querySelector(".element__title");
-        const cardImage = cardEmptyCopy.querySelector(".element__image");
-        const cardDeleteButton = cardEmptyCopy.querySelector("button");
-              cardDeleteButton.setAttribute("index", i);
+    const cardName = cardEmptyCopy.querySelector(".element__title");
+    const cardImage = cardEmptyCopy.querySelector(".element__image");
+    const cardDeleteButton = cardEmptyCopy.querySelector("button");
 
-        cardName.textContent = arr[i].name;
-        cardImage.src = arr[i].link;
+    cardName.textContent = obj.name;
+    cardImage.src = obj.link;
 
-        const buttonLike = cardEmptyCopy.querySelector(".element__button");
-        const buttonDeleteCard = cardEmptyCopy.querySelector(".element__button-delete");
-        const cardImageFull = cardEmptyCopy.querySelector(".element__image");
+    const buttonLike = cardEmptyCopy.querySelector(".element__button");
+    const buttonDeleteCard = cardEmptyCopy.querySelector(".element__button-delete");
+    const cardImageFull = cardEmptyCopy.querySelector(".element__image");
 
-        //функция добавления лайка
-        function addLike() {
-            buttonLike.addEventListener("click", function(event) {
-                event.target.classList.add("element__like_active");
-            })
-        }
-        
-        //функция удаления лайка
-        function deleteCard() {
-            buttonDeleteCard.addEventListener("click", function(event) {
-                const cardActive = this.parentNode;
-                cardActive.parentNode.removeChild(cardActive);
-                const index = this.getAttribute("index");
-                initialCards.splice(index, 1);      
-            });
-        }
-        
-        //функция открытия попапа с картинкой
-        function openFullPopupImage() {
-            cardImageFull.addEventListener("click", function(event) {
-                popupImage.classList.add("popup_opened");
-                let cardImageLink = event.target.src;
-                const img = popupImage.querySelector(".popup__image");
-                img.src = cardImageLink;
-                const subtitle = popupImage.querySelector(".popup__subtitle");
-                subtitle.textContent = this.parentNode.querySelector(".element__title").textContent;
-            })                 
-        }
-
-        addLike();
-        deleteCard();
-        openFullPopupImage();     
-        gallery.prepend(cardEmptyCopy);     
-    }
+    addLike(buttonLike);
+    deleteCard(buttonDeleteCard);
+    openFullPopupImage(cardImageFull);     
+    gallery.prepend(cardEmptyCopy);   
+         
 }
 
-createCard(initialCards);
+initialCards.forEach((obj)=>{   createCard(obj);    });
+
 
 //создаем новый объект и добавляем его в массив. createCard вызываем эту функцию - она создает карточки на основе 
 //уже нового массива
 function formAddCard(cardName, cardImage) {
-    gallery.innerHTML = "";
+    //gallery.innerHTML = "";
     const popupTitleValue = popupTitle.value;
     const popupLinkValue = popupLink.value;
 
@@ -159,7 +159,7 @@ function formAddCard(cardName, cardImage) {
     }
 
     initialCards.unshift(newObject);
-    createCard(initialCards);
+    createCard(newObject);
 }
 
 
